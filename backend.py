eedfa6cd30d1b7f8761fad8f2fff7c6824fa230c
shm <s.mudda@dautomation.com>
2023-07-21 13:57:50 +0200

    Added images to the sky and added Backend code

--- backend.py
+++ backend.py
@@ -2,17 +2,13 @@ import requests
 
 API_KEY = '798273736sdhkjahd37981731hjkh312787'
 
-def get_data(place, forcast_days, kind):
+def get_data(place, forcast_days=None):
     url = f"https://api.openweathermap.org/data/2.5/forecast?q={place}&apiid={API_KEY}"
     response = requests.get(url)
     data = response.json()
     filtered_data = data['list']
     nr_values = 8 * forcast_days
-    filtered_data = filtered_data[:nr_values]
-    if kind == 'Temperature':
-        filtered_data = [dict['main']['temp'] for dict in filtered_data]
-    if kind == 'Sky':
-        filtered_data = [dict['weather'][0]['main'] for dict in filtered_data]
+    filtered_data = filtered_data[:nr_values]        
     return filtered_data
 
 if __name__ == '__main__':
--- main.py
+++ main.py
@@ -12,7 +12,18 @@ options = st.selectbox('select data to view',
 
 st.subheader(f'{options} for the next {days} days in {place}')
 
-d, t = get_data(place, days, options)
+if place:
+    filtered_data = get_data(place, days)
 
-figure = px.line(x=d, y=t, labels={'x': 'Date', 'y': 'Temperature (C)'})
-st.plotly_chart(figure)
+if options == 'Temperature':
+    temperatures = [dict['main']['temp'] for dict in filtered_data]
+    dates = [dict['dt_txt'] for dict in filtered_data]
+    figure = px.line(x=dates, y=temperatures, labels={'x': 'Date', 'y': 'Temperature (C)'})
+    st.plotly_chart(figure)
+
+if options == 'Sky':
+    images = {'Clear': 'images/clear.png', 'Clouds': 'images/cloud.png', 
+              'Rain': 'images/rain.png', 'Snow': 'images/snow.png'}
+    sky_conditions = [dict['weather'][0]['main'] for dict in filtered_data]
+    image_path = [images[condition] for condition in sky_conditions]
+    st.image(image_path, width=110)
