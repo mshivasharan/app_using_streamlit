5c86e74b3321cccada10f958cd852db68450cd32
shm <s.mudda@dautomation.com>
2023-07-21 13:42:05 +0200

    updated code with API key from weather API

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import streamlit as st
+import plotly.express as px
+from backend import get_data
 
 st.header('Weather Forecast for the Next Days')
 place = st.text_input('Place: ')
@@ -9,3 +11,8 @@ options = st.selectbox('select data to view',
                        ('Temperature', 'Sky'))
 
 st.subheader(f'{options} for the next {days} days in {place}')
+
+d, t = get_data(place, days, options)
+
+figure = px.line(x=d, y=t, labels={'x': 'Date', 'y': 'Temperature (C)'})
+st.plotly_chart(figure)
